09f59a2d1e68af9e4dd42d6bfaba531157b3e24b
Yuvraj-Lolage <yuvilolage7@gmail.com>
2024-03-18 21:32:13 +0530

    added ATLAS link

--- db/connection.db.js
+++ db/connection.db.js
@@ -1,5 +1,5 @@
 const mong = require('mongoose');
-const DATABASE_URL = "mongodb://127.0.0.1:27017/Ecommerce";
+const DATABASE_URL = "mongodb+srv://yuvraj:yuvraj@cluster0.mamoiv2.mongodb.net/Ecommerce";
 const connection = mong.connect(DATABASE_URL);
 
 module.exports ={
